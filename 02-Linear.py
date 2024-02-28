0e5001c29000d4b07268175ec6aebeae3abc9705
Onur Küçük <124556360+OnurKck8@users.noreply.github.com>
2024-02-28 23:09:29 +0300

    Add files via upload

---
+++ 02-Linear.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+
+
+# In[11]:
+
+
+# Dosya konumuna ulaşıldı
+data=pd.read_excel('/Users/onurkck/Desktop/PythonData/Data/GeneralLearn.xlsx')
+
+
+# In[12]:
+
+
+print(data.head(3))
+
+
+# In[31]:
+
+
+# Label Encoding işlemi
+data['Region'] = data['Region'].map({'İstanbu': 1, 'Female': 0})
+
+
+# In[32]:
+
+
+# X ve y değişkenlerini belirleme
+X = data[['Product_Prise']]
+y = data[['Sales_Quantity']]
+
+# Veri setini eğitim ve test setlerine ayırma
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) # %80 eğit  %20 tahmin et
+
+# Lineer regresyon modeli oluşturma ve eğitme
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+
+# In[24]:
+
+
+# Eğitim ve test veri setleri üzerinde tahmin yapma
+y_train_pred = model.predict(X_train)
+y_test_pred = model.predict(X_test)
+
+# Eğitim ve test veri setleri için doğruluk oranı hesaplama
+accuracy_train = model.score(X_train, y_train)
+accuracy_test = model.score(X_test, y_test)
+
+r2_train = r2_score(y_train, y_train_pred)
+r2_test = r2_score(y_test, y_test_pred)
+
+print('Eğitim Seti Doğruluk Oranı:', accuracy_train)
+print('Test Seti Doğruluk Oranı:', accuracy_test)
+print('Eğitim Seti R² Değeri:', r2_train)
+print('Test Seti R² Değeri:', r2_test)
+
+
+# In[33]:
+
+
+#Neyi tahmin edeceksin
+prediction_april_sales = model.predict([[20]])
+print('Nisan satış miktarı tahmini:', prediction_april_sales)
+
+
+# In[34]:
+
+
+# Görselleştirmeye lojistik regresyon eğrisini ekleyin
+plt.scatter(data['Product_Prise'], data['Sales_Quantity'], label='Gerçek Satış')
+plt.plot(data['Product_Prise'], model.predict(X), color='red', label='Tahmini Satış')
+plt.xlabel('Product_Prise')
+plt.ylabel('Sales_Quantity')
+plt.title('Lineer Regresyon Modeli ile Tahmini Satış')
+plt.legend()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
